7b39259d8ffd8df8cc9f699ac54f1f85239d3c05
Saygin <sayginzdogu@gmail.com>
2015-02-16 20:04:38 +0200

    initial commit.
    maven support added. a dummy main class is added.

---
+++ src/main/java/tr/bilkent/oop/railwaysimulator/Main.java
@@ -0,0 +1,12 @@
+package tr.bilkent.oop.railwaysimulator;
+
+/**
+ * Created by Saygin on 16.2.2015.
+ * 
+ *  Dummy Main class. To make an initial commit to respitory. 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello World. I'm a railway simulator.");
+    }
+}
