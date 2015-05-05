78c19e7f20f1be57283e8901e5ae2e8f8ef70ccf
Elif <elifdayi@gmail.com>
2015-05-06 00:05:28 +0300

    hello world

--- src/main/java/tr/bilkent/oop/railwaysimulator/controller/MasterController.java
+++ src/main/java/tr/bilkent/oop/railwaysimulator/controller/MasterController.java
@@ -4,5 +4,7 @@ package tr.bilkent.oop.railwaysimulator.controller;
  * Created by saygin on 5/1/2015.
  */
 public class MasterController {
+    // hello world...
+
     ///Tihs is a random commit to test...
 }
