8f1bf7586b4f8bdca46c7bb2f3cfe33721c7baca
Gizem <gzmuzunr@gmail.com>
2015-05-11 01:20:20 +0300

    Structure

--- src/main/java/tr/bilkent/oop/railwaysimulator/view/SimpleStationView.java
+++ src/main/java/tr/bilkent/oop/railwaysimulator/view/SimpleStationView.java
@@ -1,7 +1,15 @@
 package tr.bilkent.oop.railwaysimulator.view;
 
+import sun.java2d.pipe.SpanShapeRenderer;
+import tr.bilkent.oop.railwaysimulator.model.railwaysystem.Station;
+
+import javax.swing.*;
+
 /**
  * Created by saygin on 5/1/2015.
  */
-public class SimpleStationView {
+public class SimpleStationView extends JPanel implements StationView{
+    public SimpleStationView(){
+        super();
+    }
 }
