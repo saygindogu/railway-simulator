a75ea09fdec5a94edeb16bc95866d2bb0e5fdd62
Gizem <gzmuzunr@gmail.com>
2015-05-11 01:20:08 +0300

    Structure

--- src/main/java/tr/bilkent/oop/railwaysimulator/view/OptionPanel.java
+++ src/main/java/tr/bilkent/oop/railwaysimulator/view/OptionPanel.java
@@ -1,9 +1,14 @@
 package tr.bilkent.oop.railwaysimulator.view;
 
 import javax.swing.*;
+import javax.swing.text.html.Option;
 
 /**
  * Created by saygin on 5/1/2015.
  */
 public class OptionPanel extends JPanel {
+
+    public OptionPanel(){
+        super();
+    }
 }
