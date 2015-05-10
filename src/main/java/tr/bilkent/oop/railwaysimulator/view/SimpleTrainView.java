16aea928d62a17d45c50eafaf22fe0fad2022d7d
Gizem <gzmuzunr@gmail.com>
2015-05-11 01:20:35 +0300

    Structure

--- src/main/java/tr/bilkent/oop/railwaysimulator/view/SimpleTrainView.java
+++ src/main/java/tr/bilkent/oop/railwaysimulator/view/SimpleTrainView.java
@@ -1,7 +1,12 @@
 package tr.bilkent.oop.railwaysimulator.view;
 
+import javax.swing.*;
+
 /**
  * Created by saygin on 5/1/2015.
  */
-public class SimpleTrainView implements TrainView {
+public class SimpleTrainView extends JPanel implements TrainView {
+    public SimpleTrainView(){
+        super();
+    }
 }
