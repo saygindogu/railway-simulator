3539676d71e30cf420756ac671b7c2681208d93b
saygin <sayginzdogu@gmail.com>
2015-05-11 14:47:36 +0300

    Uncommitted changes before update operation at 5/11/2015 2:28 PM [Default]

--- src/main/java/tr/bilkent/oop/railwaysimulator/model/user/User.java
+++ src/main/java/tr/bilkent/oop/railwaysimulator/model/user/User.java
@@ -67,6 +67,7 @@ public class User {
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
+                "id=" + identity.toString() +
                 '}';
     }
 }
